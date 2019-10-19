84a6136b6867390dfb6373421c3e9bf430982092
divyamraj18 <41264059+divyamraj18@users.noreply.github.com>
2019-10-20 02:47:21 +0530

    update linearreg.py

--- Linear Regression/linearreg.py
+++ Linear Regression/linearreg.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-dataset=pd.read_csv('Salary_data.csv')
-x=dataset.iloc[:,:-1]
-y=dataset.iloc[:,1]
+dataset=pd.read_csv('Salary_Data.csv')
+df1 = dataset.iloc[:20]
+df2 = dataset.iloc[20:]
+x_train=df1.iloc[:,:-1]
+x_test=df2.iloc[:,:-1]
+y_train=df1.iloc[:,1]
+y_test=df2.iloc[:,1]
 from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=1/3,random_state=0)
 from sklearn.linear_model import LinearRegression
 regressor=LinearRegression();
 regressor.fit(x_train,y_train)
-y_pred=regressor.predict(x_train)
-
+y_pred=regressor.predict(x_test)
