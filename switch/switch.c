2cd95bfe3e0baa33e8d6a5b8d377420ac70ad432
divyamraj18 <divyamraj18@gmail.com>
2019-10-20 01:16:25 +0530

    upadted switch

--- switch/switch.c
+++ switch/switch.c
@@ -1,46 +1,5 @@
-#include<conio.h>
-#include <stdio.h>
 #include<stdio.h>
-
-
-int main()
-{
-    int num,ch;
-    printf("enter the  two digit number of your choice");
-    scanf("%d",&num);
-    while(ch!=2){
-    printf("This is the following menu options\n");
-    printf("----------1.Choose here for incrementing the number-----------------\n");
-    printf("----------2.Choose here for decrementing the number-------------------\n");
-    printf("----------3.EXIT THE PROGRAM----------------------------\n");
-    printf("enter the choice");
-    scanf("%d",&ch);
-    
-    switch(num) {
-        case 1: {
-            int res;
-            res=incr(num);
-            printf("the incremented number is %d\n",res);
-            break;
-        }
-        case 2:{
-            int res;
-            res=decr(num);
-            printf("the decremented number is %d\n",res);
-            break;
-            
-        }
-        case 3:{
-            exit(0);
-            break;
-        }
-    }
-    }       
-    
-
-    return 0;
-}
-
+#include<stdlib.h>
 int incr(int x)
 {
      x=x+1;
@@ -51,5 +10,50 @@ int decr(int x)
     x=x-1;
     return x;
 }
+int main()
+{
+    int num,ch;
+    printf("enter the  two digit number of your choice");
+    scanf("%d",&num);
+    printf("enter the choice");
+    printf("This is the following menu options\n");
+    printf("----------1.Choose here for incrementing the number-----------------\n");
+    printf("----------2.Choose here for decrementing the number-------------------\n");
+    printf("----------3.EXIT THE PROGRAM----------------------------\n");
+    scanf("%d",&ch);
+    		while(ch!=3){
+    
+    			switch(ch) {
+        			case 1: {
+					  int res;
+					  res=incr(num);
+					  printf("the incremented number is %d\n",res);
+					  break;
+        				}
+				   case 2:{
+					  int res;
+					  res=decr(num);
+					  res=res-1;
+					  printf("the decremented number is %d\n",res);
+					  break;
+					  
+				   }
+				   case 3:{
+					  exit(0);
+					  break;
+				   }
+    			}printf("This is the following menu options\n");
+    printf("----------1.Choose here for incrementing the number-----------------\n");
+    printf("----------2.Choose here for decrementing the number-------------------\n");
+    printf("----------3.EXIT THE PROGRAM----------------------------\n");
+    printf("enter the choice");
+    scanf("%d",&ch);
+    }       
+    
+
+    return 0;
+}
+
+
 
 
